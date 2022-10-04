2f365abf2223a17a17dc289b568899a31b3a9d45
jjanmo <jjanmo07@hanmail.net>
2022-10-04 23:49:54 +0900

    feat: add app(server)

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,36 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express, { Request, Response, NextFunction, Application } from 'express'
 import * as dotenv from 'dotenv'
+import todoRouter from './controllers/todo.controller'
+
 dotenv.config()
 
-const app = express()
-const port = process.env.PORT
+class App {
+  public app: Application
+  private port: number
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello World!')
-})
+  constructor() {
+    const app = express()
+    this.app = app
+    this.port = Number(process.env.PORT) || 9331
+  }
 
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`)
-})
+  private setRouter() {
+    this.app.use(todoRouter)
+  }
+  private setMiddleware() {}
+
+  public listen() {
+    this.setRouter()
+
+    this.app.listen(this.port, () => {
+      console.log(`Server listening on http://localhost:${this.port}`)
+    })
+  }
+}
+
+function init() {
+  const app = new App()
+  app.listen()
+}
+
+init()
